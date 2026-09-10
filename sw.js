5f2f6cc4b6a840ecc040748a018a3e6940d7ff0a
kotone-dev1 <barista.kotone@gmail.com>
2026-09-11 02:21:10 +0900

    今日の予定を喋って組む、日付表示、漢数字対応

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = "daily-planner-20260910171722";
+const CACHE = "daily-planner-20260910172031";
 const ASSETS = ["./", "./index.html", "./manifest.webmanifest"];
 
 self.addEventListener("install", (e) => {
