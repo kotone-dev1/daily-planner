af15a299ca1a2735f41d7bcc894b345bb2986bca
kotone-dev1 <barista.kotone@gmail.com>
2026-09-11 01:18:42 +0900

    時計アプリ風に作り直し、スワイプ削除と4区切りを追加

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = "daily-planner-20260910160935";
+const CACHE = "daily-planner-20260910161751";
 const ASSETS = ["./", "./index.html", "./manifest.webmanifest"];
 
 self.addEventListener("install", (e) => {
