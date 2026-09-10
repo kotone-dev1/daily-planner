b19be9190991e91af7ca8eb91fb10409f1d591cc
kotone-dev1 <barista.kotone@gmail.com>
2026-09-11 01:59:49 +0900

    まとめて喋って仕分ける入口を追加、黒ベースに

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = "daily-planner-20260910162046";
+const CACHE = "daily-planner-20260910165927";
 const ASSETS = ["./", "./index.html", "./manifest.webmanifest"];
 
 self.addEventListener("install", (e) => {
