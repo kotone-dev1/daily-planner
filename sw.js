b203c504fe8113b1029083ab6373c0ed0fa98b37
kotone-dev1 <barista.kotone@gmail.com>
2026-09-11 02:03:49 +0900

    期限つきと空き時間ぶんを見分けられるように

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = "daily-planner-20260910165927";
+const CACHE = "daily-planner-20260910170312";
 const ASSETS = ["./", "./index.html", "./manifest.webmanifest"];
 
 self.addEventListener("install", (e) => {
